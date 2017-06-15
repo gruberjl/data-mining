617b05d0e2e3c17997728bd6fe3fbe6930be0b97
john <gruberjl@gmail.com>
2017-06-14 23:00:49 -0400

    'a'

--- app.js
+++ app.js
@@ -1,92 +1,46 @@
-const { join } = require('path');
-const hubspot = require(join(__dirname, 'HubSpot.js'))
-var os = require('os');
+const jsdom = require("jsdom");
+const { JSDOM, CookieJar } = jsdom;
 var request = require('request-promise');
-const key = process.env.HUBSPOT_KEY
-const parseDomain = require('parse-domain')
-const cheerio = require('cheerio')
-
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
-const getDomain = (company) => `${parseDomain(company.properties.website.value).domain}.${parseDomain(company.properties.website.value).tld}`
-
-const getExchangeVersion = (html) => {
-  const $ = cheerio.load(html)
-  const icon = $('link[rel="shortcut icon"]').prop('href')
-  if (icon) {
-      if (icon.startsWith("/owa/auth/")) {
-        const exchangeVersion = icon.split('/')[3]
-        return exchangeVersion
-      } else if (icon.startsWith('/owa/')) {
-        const exchangeVersion = icon.split('/')[2]
-        return exchangeVersion
-      }
-  }
+const username = process.env.LINKEDIN_EMAIL
+const password = process.env.LINKEDIN_PASSWORD
+const jsdomOptions = {
+  includeNodeLocations: true,
+  'userAgent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+  runScripts: "dangerously"
 }
 
-const crawlOWA = (domain, subdomain = "mail") => request({
-  uri: `https://${subdomain}.${domain}/owa`,
-  headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-      'Accept-Language': 'en-US, en;q=0.8',
-      Accept: 'text / html, application/xhtml+xml,application/xml;q=0.9, image/webp,*;q=0.8'
-  },
-  timeout: 1500
-}).then(getExchangeVersion)
+const loginCookieJar = (cookies, url="https://www.linkedin.com/uas/login-submit") => {
+  let j = request.jar();
+  cookies.split('; ').forEach((cookie) => {
+    j.setCookie(request.cookie(cookie), url);
+  })
+  return j
+}
 
-const updateExchangeVersion = (company, url, exchangeVersion) => request({
-    method: 'PUT',
-    uri: `https://api.hubapi.com/companies/v2/companies/${company.companyId}?hapikey=${key}`,
-    body: {
-        "properties": [
-            {
-                "name": "exchange_version",
-                "value": exchangeVersion
-            },
-            {
-                "name": "exchange_owa_url",
-                "value": url
-            }
-        ]
-    },
-    json: true
+const loginPost = (cookies, loginCsrfParam, session_key = username, session_password = password, isJsEnabled = false) => new Promise((res, rej) =>{
+  const url = "https://www.linkedin.com/uas/login-submit"
+  const form = {session_key, session_password, isJsEnabled, loginCsrfParam}
+  const jar = loginCookieJar(cookies)
+  request.post({url, jar, form, followAllRedirects: true}).then((page) => {
+    res({jar, form, page})
+  }, (err) => {
+    rej(err)
+  })
 })
 
-const getSetExchangeVersion = (company) => {
-  const domain = getDomain(company)
+const login = () => JSDOM.fromURL("https://www.linkedin.com/", jsdomOptions).then((dom) => {
+  const cookies = dom.window.document.cookie
+  const csrf = dom.window.document.querySelector("#loginCsrfParam-login").value
+  return loginPost(cookies, csrf)
+});
 
-  crawlOWA(domain, 'mail').then((exchangeVersion) => {
-    updateExchangeVersion(company, `mail.${domain}`, exchangeVersion)
-  }, (mailErr) => {
-    crawlOWA(domain, 'webmail').then((webmailExchangeVersion) => {
-      updateExchangeVersion(company, `webmail.${domain}`, webmailExchangeVersion)
-    }, (webmailerr) => {
-      crawlOWA(domain, 'smtp').then((smtpExchangeVersion) => {
-        updateExchangeVersion(company, `smtp.${domain}`, smtpExchangeVersion)
-      }, (smtpErr) => {
-        console.log(`${domain}: Exchange OWA URL not found`)
-      })
-    })
+login().then(({jar}) => {
+  const url = "https://www.linkedin.com/company/wusinich-brogan-&-stanzione"
+  jsdomOptions.cookieJar =  new jsdom.CookieJar(jar.store);
 
+  JSDOM.fromURL(url, jsdomOptions).then((dom) => {
+    console.log(dom.serialize())
+  }, (err) => {
+    console.log(err)
   })
-}
-
-const run = () => {
-    hubspot.getCompanies(["website", "email_provider", "spam_filter_service", "mx_records"]).then((companies) => {
-        const companiesWithMx = companies.filter((company) => Boolean(company.properties.mx_records) && !company.properties.email_provider.value)
-        companiesWithMx.forEach((company, idx) => {
-          setTimeout(() => {
-              getSetExchangeVersion(company)
-          }, idx * 500)
-        })
-    })
-}
-
-//run()
-
-crawlOWA("bazless.com", 'mail').then((exchangeVersion) => {
-  console.log(exchangeVersion)
-  //updateExchangeVersion(company, `mail.${domain}`, exchangeVersion)
-}, (err) => {
-  console.log(err)
 })
